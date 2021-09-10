74d6ffaf1948e175e28b5f4cd22bfb26a1ae13bf
DUDY206 <ninhkhuongduy206@gmail.com>
2021-09-10 11:45:59 +0700

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
 import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql', // type of our database
+      host: 'mysql', // database host
+      port: 3306, // database host
+      username: 'dudy206', // username
+      password: 'abcdef123', // user password
+      database: 'nestjs', // name of our database,
+      autoLoadEntities: true, // models will be loaded automatically 
+      synchronize: true, // your
+    })
+  ],
   controllers: [AppController, ],
   providers: [AppService],
 })
