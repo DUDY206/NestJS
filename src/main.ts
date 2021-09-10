f4763c593f6a3f9aaa3c6d23daa4875fc1e0e298
DUDY206 <ninhkhuongduy206@gmail.com>
2021-09-10 11:15:47 +0700

    validation Data Transfer Object

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    //return object dto with extractly declared properties in class dto
+    whitelist: true,
+    
+    //forbiden request when it has properties not declared in class dtio
+    forbidNonWhitelisted: true,
+
+    //transform request object to DTO object
+    transform: true,
+  }));
   await app.listen(3000);
 }
 bootstrap();
